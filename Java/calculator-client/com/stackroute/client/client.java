6cca745ac1b76528d873ebbbeefffa8e55ce7032
Deepak Patwa <deepakgkp192@gmail.com>
2018-12-19 17:22:18 +0530

    first commit

---
+++ Java/calculator-client/com/stackroute/client/client.java
@@ -0,0 +1,9 @@
+package com.stackroute.client;
+import com.stackroute.lib.*;
+public class client{
+public static void main(String args[])
+{
+	library l=new library();
+	System.out.println(l.add(5,6));
+}
+}	
---
+++ Java/calculator-library/com/stackroute/lib/library.java
@@ -0,0 +1,8 @@
+package com.stackroute.lib;
+
+public class library{
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+}
